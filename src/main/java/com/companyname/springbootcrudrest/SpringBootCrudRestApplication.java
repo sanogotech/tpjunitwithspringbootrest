653f929783dea11131dc1007628d6ffc6e56a184
sanogo souleymane <sanogotech@yahoo.fr>
2020-06-12 10:49:16 +0000

    Update

--- src/main/java/com/companyname/springbootcrudrest/SpringBootCrudRestApplication.java
+++ src/main/java/com/companyname/springbootcrudrest/SpringBootCrudRestApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class SpringBootCrudRestApplication {
 
+	//Test Travis
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudRestApplication.class, args);
 	}
